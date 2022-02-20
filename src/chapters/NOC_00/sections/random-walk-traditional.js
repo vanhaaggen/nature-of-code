b0457d98f76084f074bdac2ab27f5f6d112f7f23
vanhaaggen <vanhaaggen@gmail.com>
2022-02-20 20:35:25 +0100

    add syntax wrapper and section

--- src/chapters/NOC_00/sections/random-walk-traditional.js
+++ src/chapters/NOC_00/sections/random-walk-traditional.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ReactP5Wrapper } from 'react-p5-wrapper';
+import SyntaxHighlighterWrapper from '../../../components/SyntaxHighlightWrapper';
+import GenericWrapper from '../../../components/GenericWrapper';
 
 const RandomWalkTraditional = () => {
   const sketch = (p5) => {
     let walker;
-
     class Walker {
       constructor() {
         this.x = p5.width / 2;
@@ -12,16 +13,13 @@ const RandomWalkTraditional = () => {
         this.color = 0;
       }
 
-      r() {
-        let r = p5.random(255); // r is a random number between 0 - 255
-        let g = p5.random(100, 200); // g is a random number betwen 100 - 200
-        let b = p5.random(100); // b is a random number between 0 - 100
-        let a = p5.random(200, 255); // a is a random number between 200 - 255
-        this.color = { r, g, b, a };
+      randomColor() {
+        let color = p5.floor(p5.random() * 16777215).toString(16);
+        this.color = `#${color}`;
       }
 
       render() {
-        p5.stroke(this.color.r, this.color.g, this.color.b);
+        p5.stroke(this.color);
         p5.strokeWeight(10);
         p5.point(this.x, this.y);
       }
@@ -30,16 +28,16 @@ const RandomWalkTraditional = () => {
         const choice = p5.floor(p5.random(4));
         if (choice === 0) {
           this.x += 3;
-          this.r();
+          this.randomColor();
         } else if (choice == 1) {
           this.x -= 3;
-          this.r();
+          this.randomColor();
         } else if (choice == 2) {
           this.y += 3;
-          this.r();
+          this.randomColor();
         } else {
           this.y -= 3;
-          this.r();
+          this.randomColor();
         }
         this.x = p5.constrain(this.x, 0, p5.width - 1);
         this.y = p5.constrain(this.y, 0, p5.height - 1);
@@ -49,7 +47,7 @@ const RandomWalkTraditional = () => {
     p5.setup = () => {
       p5.createCanvas(640, 360);
       walker = new Walker();
-      p5.background(127);
+      p5.background('#e7f1ff');
     };
 
     p5.draw = () => {
@@ -57,7 +55,67 @@ const RandomWalkTraditional = () => {
       walker.render();
     };
   };
-  return <ReactP5Wrapper sketch={sketch} />;
+
+  const codeString = `
+  const RandomWalkTraditional = () => {
+    const sketch = (p5) => {
+      let walker;
+      class Walker {
+        constructor() {
+          this.x = p5.width / 2;
+          this.y = p5.height / 2;
+          this.color = 0;
+        }
+  
+        randomColor() {
+          let color = p5.floor(p5.random() * 16777215).toString(16);
+          this.color = color;
+        }
+  
+        render() {
+          p5.stroke(this.color.r, this.color.g, this.color.b, this.color.a);
+          p5.strokeWeight(10);
+          p5.point(this.x, this.y);
+        }
+  
+        step() {
+          const choice = p5.floor(p5.random(4));
+          if (choice === 0) {
+            this.x += 3;
+            this.randomColor();
+          } else if (choice == 1) {
+            this.x -= 3;
+            this.randomColor();
+          } else if (choice == 2) {
+            this.y += 3;
+            this.randomColor();
+          } else {
+            this.y -= 3;
+            this.randomColor();
+          }
+          this.x = p5.constrain(this.x, 0, p5.width - 1);
+          this.y = p5.constrain(this.y, 0, p5.height - 1);
+        }
+      }
+  
+      p5.setup = () => {
+        p5.createCanvas(640, 360);
+        walker = new Walker();
+        p5.background(127);
+      };
+  
+      p5.draw = () => {
+        walker.step();
+        walker.render();
+      };
+    };
+  `;
+  return (
+    <GenericWrapper>
+      <ReactP5Wrapper sketch={sketch} />
+      <SyntaxHighlighterWrapper>{codeString}</SyntaxHighlighterWrapper>
+    </GenericWrapper>
+  );
 };
 
 export default RandomWalkTraditional;
